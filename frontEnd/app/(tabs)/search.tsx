e4a4e1003f0d788ea6ac05cbd7792bf19d468cf5
Karim Rock <153375776+Karimsayed2002@users.noreply.github.com>
2024-05-12 01:58:17 +0300

    search bar

--- frontEnd/app/(tabs)/search.tsx
+++ frontEnd/app/(tabs)/search.tsx
@@ -8,7 +8,28 @@ const page = () => {
     </View>
   );
 };
+/*this is spartaaaaaaaaaaaa*/
+export default page;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const page = () => {
+  return (
+    <View style={styles.container}>
+      <Text>search</Text>
+    </View>
+  );
+};
+/*this is spartaaaaaaaaaaaa*/
 export default page;
 
 const styles = StyleSheet.create({
